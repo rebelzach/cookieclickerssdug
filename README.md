0ff40e385896015e921b3bb2cc077bc3b6f5b410
Zach Starkebaum <zach@relevantwalk.com>
2019-06-14 10:44:07 -0700

    It works I guess

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cookieclickerssdug
 a cookie clicker challenge done at an ssdug coding challenge
+
+Run it from the `cookieclicker` directory
+`node cookiemath.js`
--- cookieclicker/cookiemath.js
+++ cookieclicker/cookiemath.js
@@ -1,46 +1,75 @@
-const cookieFarmCost = 500;
-const cookieFarmRate = 4.0;
-const goal = 2000.0;
+const fs = require('fs');
+ 
+const contents = fs.readFileSync('B-large-practice.in', 'utf8');
+const lines = contents.split('\n');
+const testCount = parseInt(lines[0]);
 
-function doCookie() {
-  debugger;
+let cookieFarmCost;
+let cookieFarmRate;
+let goal;
+let output = "";
 
-  let minimumFound = false;
-  let currentTime = 0;
-  let currentRate = 2.0;
-  let numberOfCookies = 0;
+for (let line = 1; line < testCount + 1; line++) {
+    const vars = lines[line].split(" ");
+    cookieFarmCost = parseFloat(vars[0]);
+    cookieFarmRate = parseFloat(vars[1]);
+    goal = parseFloat(vars[2]);
 
-  function secondsToGoal(rate, localGoal) {
-    return localGoal / rate;
-  }
+    console.log(cookieFarmCost, cookieFarmRate, goal);
 
-  const timeToBuyFarm = secondsToGoal(
-    currentRate + cookieFarmRate,
-    cookieFarmCost
-  );
-  const timeToGoal = secondsToGoal(currentRate, goal);
+    const minimum = getMinimumTime();
+    
+    output += `Case #${line}: ${minimum}\n`;
 
-  if (timeToGoal <= timeToBuyFarm) {
-    console.log("Done!");
-  } else {
-    currentTime = timeToBuyFarm;
-    currentRate += cookieFarmRate;
-    console.log("Not yet...");
-    while (!minimumFound) {
-      const timeToGoalWithNewFarm = secondsToGoal(
-        currentRate + cookieFarmRate,
-        cookieFarmCost
-      );
-      const timeToGoalDirect = secondsToGoal(currentRate, goal);
-      if (timeToGoalDirect <= timeToGoalWithNewFarm) {
-        currentTime += timeToGoalDirect;
-        minimumFound = true;
-      } else {
-        currentTime += timeToGoalWithNewFarm;
-        currentRate += cookieFarmRate;
-      }
-    }
-  }
-
-  console.log(currentTime);
+    fs.writeFileSync('output.out', output);
 }
+
+console.log(output);
+
+function getSecondsWhenBuyingFarms(numberOfFarmsToBuy)
+{
+    let currentTime = 0;
+    let currentRate = 2.0;
+
+    for(let i = 0; i < numberOfFarmsToBuy; i++) {
+        // Buy a farm
+        const timeToBuyFarm = secondsToGetSomeCookies(currentRate, cookieFarmCost);
+        currentTime += timeToBuyFarm;
+        currentRate += cookieFarmRate;
+    }
+
+    const timeToGoalDirect = secondsToGetSomeCookies(currentRate, goal);
+    return currentTime + timeToGoalDirect;
+}
+
+function secondsToGetSomeCookies(rate, localGoal) {
+    return localGoal / rate;
+}
+
+function getMinimumTime() {
+    let minimumFound = false;
+    let currentFarmBuyCount = 1;
+
+    const timeToGoalDirect = getSecondsWhenBuyingFarms(0);
+
+    let previousTime = timeToGoalDirect;
+
+    while (!minimumFound) {
+        const timeToGoalForCurrentBuyCount = getSecondsWhenBuyingFarms(currentFarmBuyCount); 
+
+        // Find the rate of improvement
+        const delta = timeToGoalForCurrentBuyCount - previousTime; 
+        //console.log(delta);
+        // If delta is greater than our "convergence" threshold then we have found our minimum
+        if (delta > -0.000000001) {
+            minimumFound = true;
+        } else {
+            // Try and buy another farm, see if that is an improvement
+            previousTime = timeToGoalForCurrentBuyCount;
+            ++currentFarmBuyCount;
+        }
+    }
+    return previousTime;
+}
+
+
